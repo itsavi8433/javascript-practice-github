44d9fe563d6a576d0b670b778c8698c730025cb2
itsavi8433 <hindolabhishek8433@gmail.com>
2024-09-03 05:17:18 +0000

    Variables let,var,const

---
+++ 01_Basics_javascript/variables.js
@@ -0,0 +1,27 @@
+// let var const Variables
+
+const accountId = 8433;
+
+// console.log(accountId);
+ let accountName = "Abhishek Singh";
+ var accountEmail = "hindolabhishek8433@gmail.com";
+ accountCity = "Mathura";
+
+//  console.table([accountId ,accountName ,accountEmail ,accountState]);
+
+//  accountId = 8126;
+//  console.log(accountId);
+
+// accountName = "Sumit Singh";
+// accountEmail ="avi19072002@gmail.com";
+// accountState = "Noida";
+
+// console.table([accountId ,accountName ,accountEmail ,accountState]);
+
+var accountEmail = "avi19072002@gmail.com";
+ let accountState;
+
+console.table([accountEmail,accountState]);
+
+
+ 
