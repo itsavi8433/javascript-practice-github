f1112460e911b1a4355e0dc1d91c0c691feac104
Abhishek singh <hindolabhishek8433@gmail.com>
2024-09-22 10:33:43 +0000

    number and Maths

---
+++ 01_Basics_javascript/num_and_maths.js
@@ -0,0 +1,38 @@
+let score = 300;         //automatically define the datatype
+// console.log(score);
+
+const balance = new Number(score);  // in this we specified the datatype of a varaiable
+// console.log(balance);
+
+let marks = 66.66666666;
+// console.log(marks.toFixed(3));
+// console.log(marks.toFixed(2));
+
+
+let marks1 = 89.67;
+
+// console.log(marks1.toString().length);
+// console.log(typeof marks1.toString()); /* datatype = string */
+
+let othermarks = 23.5678;
+
+// console.log(othermarks.toPrecision(3))   // 23.6
+
+// console.log(othermarks.toPrecision(5));  //23.568
+
+let anothernumber = 1345.5674      
+
+// console.log(anothernumber.toPrecision(3));
+// console.log(anothernumber.toPrecision(2))  //1.3e+4
+
+
+let hundered = 100000000
+
+// console.log(hundered.toLocaleString());    // o/p value comes based on Us standard
+// console.log(hundered.toLocaleString("en-IN"));   // o/p value comes based on indian standard
+
+
+//*******************************Maths****************************************************** */
+
+
+
