5b4ea7959332f71dac3cfa0db9a72a083c62fc93
Abhishek singh <hindolabhishek8433@gmail.com>
2024-09-22 06:02:51 +0000

    strings

--- 01_Basics_javascript/string.js
+++ 01_Basics_javascript/string.js
@@ -1,33 +1,75 @@
-let name = "Abhishek Singh";
-let repocount = 50;
+// let name = "Abhishek Singh";
+// let repocount = 50;
 
-// console.log(name + repocount + " value");  --> use not this type of syntax in mordern era
+// // console.log(name + repocount + " value");  --> use not this type of syntax in mordern era
 
-// console.log(`Hello Myself ${name} and my repocount is $ {repocount}`);  // use this syntax in string
+// // console.log(`Hello Myself ${name} and my repocount is $ {repocount}`);  // use this syntax in string
 
-const Gamename = new String("abhisheksingh");   // use object in javascript  
+// const Gamename = new String("abhisheksingh");   // use object in javascript  
 
-console.log(Gamename[6]);    // we can put key 
-// console.log(Gamename[s]); // we can put value as this
+// console.log(Gamename[6]);    // we can put key 
+// // console.log(Gamename[s]); // we can put value as this
 
-// console.log(Gamename.__proto__);
-// console.log(Gamename.length);
-// console.log(Gamename.toUpperCase());
-// console.log(Gamename.toLowerCase());
-// console.log(Gamename.charAt(5));
-// console.log(Gamename.indexOf("i"));
+// // console.log(Gamename.__proto__);
+// // console.log(Gamename.length);
+// // console.log(Gamename.toUpperCase());
+// // console.log(Gamename.toLowerCase());
+// // console.log(Gamename.charAt(5));        //put key not value
+// // console.log(Gamename.indexOf("i"));      //put value in double quote not key
 
-// console.log(Gamename.substring(3 , 7));   //ishe    // last value not inclued i.e 7
+// // console.log(Gamename.substring(3 , 7));   //ishe    // last value not inclued i.e 7
 
-console.log(Gamename.slice(2, 4));
+// console.log(Gamename.slice(2, 4));
 
-let newstring = "    sumitsingh     ";
-console.log(newstring);
-console.log(newstring.trim());
+// let newstring = "    sumitsingh     ";
+// console.log(newstring);
+// console.log(newstring.trim());
 
-let url ="https://abhishek.com/abhishek%20singh"
+// let url ="https://abhishek.com/abhishek%20singh"
 
-console.log(url.replace("%20" , "_"));
-console.log(url.includes("%20"));
-console.log(url.includes("abhi"));
-console.log(url.includes("kumar"));
+// console.log(url.replace("%20" , "_"));
+// console.log(url.includes("%20"));
+// console.log(url.includes("abhi"));
+// console.log(url.includes("kumar"));
+
+
+//************************************************************************************** */
+
+//strings
+
+let name = "Abhishek singh";
+let hobby1 = "Playing cricket";
+let hobby2 = "Reading Books"
+
+// console.log(name + hobby1 +" Reading Books");
+
+//  console.log(`my name is ${name} and my Hobbies are ${hobby1} and ${hobby2}`);
+
+let Gamename = new String("Call of Duty");
+
+//  console.log(Gamename.toUpperCase());
+//  console.log(Gamename.__proto__)
+//  console.log(Gamename.indexOf(" "));
+//  console.log(Gamename.indexOf("D"));
+//  console.log(Gamename.includes("Call"));
+//  console.log(Gamename.includes(" "))
+//  console.log(Gamename.includes("ram"))
+
+ let newstring1 = "Army_school_mathura_Cantt"
+//  console.log(newstring1.replace("_", "-"));
+
+ let newstring2 ="      harsh     ";
+//  console.log(newstring2)
+//  console.log(newstring2.trim());
+
+ let newstring3 ="sumit singh";
+
+//  console.log(newstring3.slice(4,7))   //t s
+//  console.log(newstring3.substring(2,8)); //mit si
+//  console.log(newstring3.charAt(6));    //s
+// console.log(newstring3.anchor("sumit"));
+console.log(newstring3.split(" "));
+
+
+let newstring4 = "Abhishek-ab-com"
+console.log(newstring4.split("-"));
